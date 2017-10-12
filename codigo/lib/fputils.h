57026436808821bf4808b35cb814e837ab0b9c98
Angel Yan <angelyan96@gmail.com>
2017-10-12 00:34:39 -0300

    Pongo fputils en un solo archivo

--- codigo/lib/fputils.h
+++ codigo/lib/fputils.h
@@ -1,13 +1,17 @@
 #ifndef __FPUTILS_H__
 #define __FPUTILS_H__
 
+#include <cmath>
+
 using namespace std;
 
 namespace fputils {
+    
+    const double EPS = 10e-10;
 
-    bool eq(const double a, const double b);
+    inline bool eq(const double a, const double b) { return abs(a - b) <= EPS; };
 
-    bool lt(const double a, const double b);
+    inline bool lt(const double a, const double b) { return a - b <= EPS; };
 }
 
 #endif //__FPUTILS_H__
