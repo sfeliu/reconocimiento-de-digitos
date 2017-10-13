3c3ec03435acfb7c154195f64c6fc8e7001933eb
Angel Yan <angelyan96@gmail.com>
2017-10-13 13:16:10 -0300

    Corrijo cross validation

--- codigo/cross_validation.cpp
+++ codigo/cross_validation.cpp
@@ -10,23 +10,37 @@
 
 using namespace std;
 
-void KFold(
-    const unsigned int k_KFold,
-    OCR &ocr,
-    OCR::base_de_datos_t &bd_train,
-    const OCR::datos_t &datos_train,
-    const char* ruta_salida
-    ) {
+char* obtener_ruta(const string prefijo, const unsigned int f, const unsigned int a, const unsigned int k) {
+    ostringstream ss;
+    ss << prefijo << "_f" << f << "_a" << a << "_k" << k << ".csv";
+    string str = ss.str();
+    unsigned int len = str.length();
+    char *ruta_salida = new char[len + 1];
+    str.copy(ruta_salida, len);
+    ruta_salida[len] = '\0';
+    return ruta_salida;
+}
+
+void KFold(const unsigned int k_KFold, OCR::base_de_datos_t &bd_train, const vector<unsigned int> &PCAs,
+           const vector<unsigned int> &KNNs, const string prefijo) {
     
-    // Obtengo el tamanio base de los folds y la cantidad de elementos restantes
-    if (datos_train.size() == 0) throw runtime_error("No hay datos.");
-    unsigned int tam_datos = datos_train[0].size();
-    if (tam_datos == 0) throw runtime_error("No hay datos.");
+    if (OCR::cant_datos(bd_train) == 0 && OCR::tam_datos(bd_train) == 0) throw runtime_error("No hay datos.");
     
-    // Abro archivo de salida
-    ofstream archivo_salida(ruta_salida);
-    if (!archivo_salida.is_open()) throw runtime_error("No se puede escribir el archivo");
-    archivo_salida << "iteracion,clase,prediccion" << endl;
+    unsigned int tam_PCAs = PCAs.size();
+    unsigned int tam_KNNs = KNNs.size();
+    
+    // Creo archivos de salida
+    for (unsigned int i = 0; i < tam_PCAs; ++i) {
+        for (unsigned int j = 0; j < tam_KNNs; ++j) {
+            
+            char* ruta_salida = obtener_ruta(prefijo, k_KFold, PCAs[i], KNNs[j]);
+            ofstream archivo_salida(ruta_salida);
+            delete ruta_salida;
+            if (!archivo_salida.is_open()) throw runtime_error("No se puede escribir el archivo");
+            archivo_salida << "fold,clase,prediccion" << endl;
+            archivo_salida.close();
+        }
+    }
     
     // Permuto los indices de la base de datos aleatoriamente
     for (auto it = bd_train.begin(); it != bd_train.end(); ++it)
@@ -36,66 +50,73 @@ void KFold(
     for (unsigned int k = 0; k < k_KFold; ++k) {
         
         OCR::base_de_datos_t bd_test;
-        OCR::datos_t datos_test;
         
         // Obtengo fold de testing de cada clase
         for (auto it = bd_train.begin(); it != bd_train.end(); ++it) {
             
-            vector<OCR::indice_t> &clase = it->second;
+            auto &datos_train = it->second;
             
             // Determino tamanio del fold
-            unsigned int cant_datos = clase.size();
+            unsigned int cant_datos = datos_train.size();
             unsigned int restantes = cant_datos % k_KFold;
             unsigned int tam_fold = cant_datos / k_KFold + (k < restantes);
             
             // Saco fold para testing
-            auto d = clase.begin();
-            auto h = d + tam_fold;
-            auto ins = bd_test.insert(make_pair(it->first, vector<OCR::indice_t>(d, h)));
-            vector<OCR::indice_t> &indices_test = ins.first->second;
-            clase.erase(d, h);
-            
-            // Obtengo datos de testing
-            for (unsigned int i = 0; i < tam_fold; ++i) {
-                datos_test.push_back(OCR::dato_t(tam_datos));
-                for (unsigned int j = 0; j < tam_datos; ++j)
-                    datos_test.back()[j] = datos_train[indices_test[i]][j];
-            }
+            auto desde = datos_train.begin();
+            auto hasta = desde + tam_fold;
+            bd_test.insert(make_pair(it->first, OCR::datos_t(desde, hasta)));
+            datos_train.erase(desde, hasta);
         }
         
         // Testeo OCR
-        ocr.base_de_datos(bd_train);
-        vector<OCR::clave_t> resultados = ocr.reconocer(datos_test);
+        OCR ocr(bd_train, PCAs[0], KNNs[0]);
         
-        // Escribo resultados
-        auto res_it = resultados.begin();
-        for (auto it = bd_test.begin(); it != bd_test.end(); ++it) {
+        for (unsigned int i = 0; i < tam_PCAs; ++i) {    
+            ocr.alpha_PCA(PCAs[i]);
             
-            OCR::clave_t clave = it->first;
-            vector<OCR::indice_t> &clase = it->second;
-            unsigned int cant_elem = clase.size();
-            
-            for (unsigned int i = 0; i < cant_elem; ++i) {
-                archivo_salida << k << ',' << clave << ',' << *res_it << endl;
-                ++res_it;
+            for (unsigned int j = 0; j < tam_KNNs; ++j) {
+                ocr.k_KNN(KNNs[j]);
+                
+                map<OCR::clave_t, vector<OCR::clave_t>> res = ocr.reconocer(bd_test);
+        
+                // Escribo resultados
+                for (auto it = res.begin(); it != res.end(); ++it) {
+                    
+                    // Abro el archivo
+                    char* ruta_salida = obtener_ruta(prefijo, k_KFold, PCAs[i], KNNs[j]);
+                    ofstream archivo_salida(ruta_salida, std::ios_base::app); // agrego, no sobreescribo
+                    delete ruta_salida;
+                    if (!archivo_salida.is_open()) throw runtime_error("No se puede escribir el archivo");
+                    
+                    // Escribo valores
+                    OCR::clave_t clave = it->first;
+                    auto &resultados = it->second;
+                    unsigned int cant_elem = resultados.size();
+                    
+                    for (unsigned int i = 0; i < cant_elem; ++i)
+                        archivo_salida << k << ',' << clave << ',' << resultados[i] << endl;
+    
+                    // Cierro el archivo
+                    archivo_salida.close();
+                }
             }
         }
         
         // Restauro la base de entrenamiento
         for (auto it = bd_test.begin(); it != bd_test.end(); ++it) {
             
-            auto &indices_test = it->second;
-            auto &clase = bd_train[it->first];
-            clase.insert(clase.end(), indices_test.begin(), indices_test.end());
+            auto &datos_test = it->second;
+            auto &datos_train = bd_train[it->first];
+            datos_train.insert(datos_train.end(), datos_test.begin(), datos_test.end());
+            datos_test.clear();
         }
     }
-    
-    archivo_salida.close();
 }
 
 int main(int argc, const char *argv[]) {
     
     // Parseo argumentos
+    
     TCLAP::CmdLine cmd("Hace cross validation del OCR", ' ', "");
     TCLAP::ValueArg<string> arg_train("t", "train", "Ruta del archivo de training", true, "", "string", cmd);
     TCLAP::ValueArg<string> arg_prefijo("p", "prefijo", "Prefijo de los archivos de salida", false, "cv", "string", cmd);
@@ -122,35 +143,26 @@ int main(int argc, const char *argv[]) {
         
     string prefijo = arg_prefijo.getValue();
     
+    
     // Leo datos de entrenamiento
     OCR::base_de_datos_t bd_train;
-    OCR::datos_t datos_train;
-    leer_datos_train(arg_train.getValue().c_str(), bd_train, datos_train);
+    leer_datos_train(arg_train.getValue().c_str(), bd_train);
     
-    // Inicializo OCR
-    OCR::base_de_datos_t bd_vacia;
-    OCR ocr(bd_vacia, datos_train, PCA_max, 0);
+    
+    // Creo listas de parametros para el OCR
+    
+    unsigned int cant_KNNs = KNN_max - KNN_min + 1;
+    vector<unsigned int> KNNs(cant_KNNs);
+    for (unsigned int i = 0; i < cant_KNNs; ++i) KNNs[i] = KNN_min + i;
+        
+    unsigned int cant_PCAs = PCA_max - PCA_min + 1;
+    vector<unsigned int> PCAs(cant_PCAs);
+    for (unsigned int i = 0; i < cant_PCAs; ++i) PCAs[i] = PCA_max - i;
+    
     
     // Hago cross validation
     for (unsigned int f = KFold_min; f <= KFold_max; ++f) {
-        for (unsigned int a = PCA_max; a >= PCA_min; --a) {
-            for (unsigned int k = KNN_min; k <= KNN_max; ++k) {
-                
-                printf("k-KFold = %u, alpha-PCA = %u, k-KNN = %u\n", f, a, k);
-                
-                // Defino ruta de salida
-                ostringstream ss;
-                ss << prefijo << "_f" << f << "_a" << a << "_k" << k << ".csv";
-                string str = ss.str();
-                char ruta_salida[str.length()];
-                str.copy(ruta_salida, str.length());
-                
-                // Hago cross validation
-                ocr.alpha_PCA(a);
-                ocr.k_KNN(k);
-                KFold(f, ocr, bd_train, datos_train, ruta_salida);
-            }
-        }
+        KFold(f, bd_train, PCAs, KNNs, prefijo);
     }
 
     return 0;
