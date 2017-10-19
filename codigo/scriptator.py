6581ab16ef20c16dafa7f26259e6d53e2447e1cf
Santiago Feliu <sfeliu@dc.uba.ar>
2017-10-19 17:08:38 -0300

    Un poco mas a conclusiones

--- codigo/scriptator.py
+++ codigo/scriptator.py
@@ -1,7 +1,7 @@
 import subprocess, os, re
 
 
-for file in os.listdir('cv_K_distinto/'):
+for file in os.listdir('cv/'):
     test = subprocess.Popen('python creadorImagen.py ' + file + ' False', stdout=subprocess.PIPE, shell=True)
     stdout = test.communicate()[0].decode()
     print(stdout)
